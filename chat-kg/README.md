9d4a0cee76465a12288a8bc31363db10a151f3b0
星星小染 <jwwang_mail@163.com>
2025-12-18 21:52:39 +0800

    Modify The Port

--- chat-kg/README.md
+++ chat-kg/README.md
@@ -1,5 +1,5 @@
 # 部署
-
+先在 vite.config.js 中修改端口 
 ```bash
 npm install 
 npm run server
--- chat-kg/vite.config.js
+++ chat-kg/vite.config.js
@@ -15,7 +15,8 @@ export default defineConfig({
   server: {
     proxy: {
       '^/api': {
-        target: 'http://172.21.173.226:8000',
+        // 修改为本地端口
+        target: 'http://127.0.0.1:8000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
